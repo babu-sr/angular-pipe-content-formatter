c6fb6ea8265639ef9881caa6ca3a37e9f0700cbd
Babu <32696294+babu-sr@users.noreply.github.com>
2020-09-05 21:42:36 +0530

    Delete content-formatter.pipe.spec.ts

--- content-formatter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ContentFormatterPipe } from './content-formatter.pipe';
-
-describe('ContentFormatterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ContentFormatterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
